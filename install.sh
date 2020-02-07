fdb6523d24d485ce058ac28702c0982098e25c9d
Thomas Leiter <thomas.leiter@outlook.at>
2020-02-07 15:05:33 +0100

    added install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -so- https://raw.githubusercontent.com/fasky-software/react_vscode_snippets/master/Handy%20React%20Snippets.code-snippets > ~/Library/Application\ Support/Code/User/snippets/Handy\ React\ Snippets.code-snippets
+echo -e "Done!"
